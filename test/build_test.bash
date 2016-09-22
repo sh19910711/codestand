ee6390013ff487d0488d020437d504c10d8b44f9
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-22 23:10:57 +0000

    fix tests

--- test/build_test.bash
+++ test/build_test.bash
@@ -7,7 +7,7 @@ should_not() {
 }
 
 docker-compose exec mockup apk add curl
-docker-compose exec mockup curl http://gateway/build/builds | should_not builds
-docker-compose exec mockup curl http://gateway/build/builds | should "403 Forbidden"
+docker-compose exec mockup curl http://gateway/builds | should_not builds
+docker-compose exec mockup curl http://gateway/builds | should "403 Forbidden"
 docker-compose exec mgmt apk add curl
-docker-compose exec mgmt curl http://gateway/build/builds | should builds
+docker-compose exec mgmt curl http://gateway/builds | should builds
