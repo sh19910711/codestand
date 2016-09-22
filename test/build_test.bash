7ed9d78a45af79d30a25493d84950252df22899f
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-22 23:21:59 +0000

    edit build_test

--- test/build_test.bash
+++ test/build_test.bash
@@ -1,13 +1,9 @@
 should() {
   grep "$@" || exit 1
 }
-
 should_not() {
   grep "$@" && exit 1
 }
 
-docker-compose exec mockup apk add curl
-docker-compose exec mockup curl http://gateway/builds | should_not builds
-docker-compose exec mockup curl http://gateway/builds | should "403 Forbidden"
-docker-compose exec mgmt apk add curl
-docker-compose exec mgmt curl http://gateway/builds | should builds
+docker-compose exec mockup sh -c "apk add curl; curl http://gateway/builds" | should_not builds
+docker-compose exec mgmt sh -c "apk add curl; curl http://gateway/builds" | should builds
