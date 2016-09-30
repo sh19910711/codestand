888c4b7aa52a24bde1ae024d4ddc0c11a679217f
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-30 22:30:01 +0000

    remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Usage (recommend)
-# $ ssh -L localhost:12345:localhost:12345 target-server
-# $ DOCKER_HOST=tcp://localhost:12345 ./deploy.sh
-
-docker-compose build && docker-compose up --remove-orphans -d
-docker-compose exec mgmt bundle exec rails db:migrate
