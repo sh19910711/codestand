ef7af523cdafdd342367fccf7bf4caabf44d81f0
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-01 00:22:29 +0000

    remove

--- test/build_test.bash
+++
@@ -1,9 +0,0 @@
-should() {
-  grep "$@" || exit 1
-}
-should_not() {
-  grep "$@" && exit 1
-}
-
-docker-compose exec mockup sh -c "apk add curl; curl http://gateway/builds" | should_not builds
-docker-compose exec mgmt sh -c "apk add curl; curl http://gateway/builds" | should builds
